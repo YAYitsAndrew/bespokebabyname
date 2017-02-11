683f15ca5cedad8c1eb373b8599c8409fb1ea7dd
Andrew Pellerano <yayitsandrew@gmail.com>
2017-02-11 15:20:41 -0800

    generate baby names using a list of approved emoji

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+var generateName = function () {
+  var idx = parseInt(Math.random() * shortnames.length);
+  var emoji1 = shortnames[idx];
+  idx = parseInt(Math.random() * shortnames.length);
+  var emoji2 = shortnames[idx];
+  var elem = $('#js_emojiContainer')[0];
+  elem.innerHTML = emojione.shortnameToImage(emoji1 + emoji2);
+};
+
+$(document).ready(function () {
+  emojione.imageType = 'svg';
+  emojione.sprites = true;
+  emojione.imagePathSVGSprites = 'assets/img/emojione.sprites.svg';
+
+  generateName();
+});
